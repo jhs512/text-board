2158fcc9c4059f4e7ecddb41dba0eb7134d8ca43
jhs512 <jangka512@gmail.com>
2020-10-10 14:39:11 +0900

    JDBC 인서트 테스트

--- src/com/sbs/example/textboard/test/JDBCConnectionTest.java
+++ src/com/sbs/example/textboard/test/JDBCConnectionTest.java
@@ -2,19 +2,31 @@ package com.sbs.example.textboard.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JDBCTest {
+public class JDBCConnectionTest {
 	public static void main(String[] args) {
 		Connection conn = null;
+		PreparedStatement pstat = null;
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
 			conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
-			
-			
+
+			String sql = "INSERT INTO article";
+			sql += " SET regDate = NOW()";
+			sql += ", updateDate = NOW()";
+			sql += ", title = CONCAT(\"제목\", RAND())";
+			sql += ", `body` = CONCAT(\"내용\", RAND());";
+
+			pstat = conn.prepareStatement(sql);
+			int affectedRows = pstat.executeUpdate();
+
+			System.out.println("affectedRows : " + affectedRows);
+
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
 		} catch (SQLException e) {
@@ -27,6 +39,15 @@ public class JDBCTest {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+
+			try {
+				if (pstat != null && !pstat.isClosed()) {
+					pstat.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		}
 	}
 }
