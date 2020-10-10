e4198ae28d66bae9eb58979c27187c592b78fd91
jhs512 <jangka512@gmail.com>
2020-10-10 13:17:57 +0900

    기본적인 게시물 리스팅, 추가 기능 구현

--- src/com/sbs/example/textboard/Main.java
+++ src/com/sbs/example/textboard/Main.java
@@ -9,14 +9,44 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 
 		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
 
 		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = scanner.nextLine();
 			cmd = cmd.trim();
 
-			if (cmd.equals("system exit")) {
-				System.out.println("프로그램을 종료합니다.");
+			if (cmd.equals("article add")) {
+				int id = lastArticleId + 1;
+				String title;
+				String body;
+
+				System.out.println("== 게시글 생성 ==");
+				System.out.printf("제목 : ");
+				title = scanner.nextLine();
+				System.out.printf("내용 : ");
+				body = scanner.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				lastArticleId++;
+
+				System.out.println(article);
+			} else if (cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+
+				if (articles.size() == 0) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+
+				System.out.println("번호 / 제목");
+
+				for (Article article : articles) {
+					System.out.printf("%d / %s\n", article.id, article.title);
+				}
+			} else if (cmd.equals("system exit")) {
+				System.out.println("== 프로그램 종료 ==");
 				break;
 			}
 		}
