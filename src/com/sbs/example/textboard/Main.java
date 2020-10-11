02ef3527f7fbc08176f040c1a952e7917130d860
jhs512 <jangka512@gmail.com>
2020-10-11 12:38:40 +0900

    게시물 작성시 DB에 저장

--- src/com/sbs/example/textboard/Main.java
+++ src/com/sbs/example/textboard/Main.java
@@ -1,5 +1,9 @@
 package com.sbs.example.textboard;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -29,9 +33,49 @@ public class Main {
 
 				Article article = new Article(id, title, body);
 				articles.add(article);
-				lastArticleId++;
 
-				System.out.println(article);
+				Connection conn = null;
+				PreparedStatement pstat = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = \"" + title + "\"";
+					sql += ", `body` = \"" + body + "\";";
+
+					pstat = conn.prepareStatement(sql);
+					pstat.executeUpdate();
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+				}
+
+				lastArticleId++;
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
