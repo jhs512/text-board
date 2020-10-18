05eb30b8886af2661162067e31b5ec49dd9549ff
jhs512 <jangka512@gmail.com>
2020-10-18 13:35:04 +0900

    회원가입 기능

--- db.sql
+++ db.sql
@@ -1,6 +1,11 @@
+# DB 생성
 DROP DATABASE IF EXISTS text_board;
 CREATE DATABASE text_board;
 
+# DB 선택
+USE text_board;
+
+# 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -8,3 +13,14 @@ CREATE TABLE article (
     title CHAR(100) NOT NULL,
     `body` TEXT NOT NULL
 );
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(200) NOT NULL
+);
+
