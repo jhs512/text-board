c79b25523f414e8dcc652349e52db9a293f3baf0
jhs512 <jangka512@gmail.com>
2020-10-24 14:48:43 +0900

    게시물 리스트에서 작성자명이 나오도록

--- src/com/sbs/example/textboard/dto/Article.java
+++ src/com/sbs/example/textboard/dto/Article.java
@@ -6,35 +6,28 @@ public class Article {
 	public int id;
 	public String regDate;
 	public String updateDate;
+	public int memberId;
 	public String title;
 	public String body;
 
-	public Article(int id, String regDate, String updateDate, String title, String body) {
-		this.id = id;
-		this.regDate = regDate;
-		this.updateDate = updateDate;
-		this.title = title;
-		this.body = body;
-	}
-
-	public Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
-	}
+	public String extra__writer;
 
 	public Article(Map<String, Object> articleMap) {
 		this.id = (int) articleMap.get("id");
-		this.title = (String) articleMap.get("title");
-		this.body = (String) articleMap.get("body");
 		this.regDate = (String) articleMap.get("regDate");
 		this.updateDate = (String) articleMap.get("updateDate");
+		this.memberId = (int) articleMap.get("memberId");
+		this.title = (String) articleMap.get("title");
+		this.body = (String) articleMap.get("body");
+
+		if (articleMap.get("extra__writer") != null) {
+			this.extra__writer = (String) articleMap.get("extra__writer");
+		}
 	}
 
 	@Override
 	public String toString() {
-		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
-				+ ", body=" + body + "]";
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", memberId=" + memberId
+				+ ", title=" + title + ", body=" + body + "]";
 	}
-
 }
